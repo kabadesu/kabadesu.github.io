40ab35e0eba865714a430cbc7088f9a2177ad211
Murray Nuttall <murray.nuttall@gmail.com>
2019-12-05 10:31:04 +1300

    Playing around with gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Img from 'gatsby-image'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+import Layout from "@layouts/layout"
+import SEO from "@components/seo"
+
+const IndexPage = (props) => (
+    <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Img fluid={props.data.placeholderImage.childImageSharp.fluid} />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
 )
 
 export default IndexPage
+
+export const PageQuery = graphql`
+    query {
+        placeholderImage: file(
+            relativePath: { eq: "gatsby-astronaut.png" }
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
